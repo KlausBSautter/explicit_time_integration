a4a849ce9bb5273921d64e0db7ee7197aaf4ed8e
KlausBSautter <sautter.klaus@gmail.com>
2017-11-20 12:09:13 +0100

    changed name of main solver file

--- testCases/import_custom_modules.py
+++ testCases/import_custom_modules.py
@@ -3,6 +3,6 @@ sys.path.append("../")
 import numpy as np
 import truss_element_non_linear_functions as truss_nl
 import truss_element_linear_functions as truss
-import solver_functions as solver
+import solver 
 import general_functions as general
 import explicit_functions as explicit
